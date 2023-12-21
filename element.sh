60849c9265c7e1c80b6f53b0516b0ea292e62131
NadirAli90 <nadiraliunar360@gmail.com>
2023-12-21 02:45:17 -0500

    fix: psql variable

--- element.sh
+++ element.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-
+PSQL="psql --username=freecodecamp --dbname=periodic_table -c"
 if [[ -z $1 ]]; then
   echo "Please provide an element as an argument."  
+fi
+
+if [[ $1 =~ ^[0-9]+$ ]]; then
+SYMBOL=$($PSQL "")
 fi
