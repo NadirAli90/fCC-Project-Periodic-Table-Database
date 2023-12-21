041addf91a9fdaf7fe1e502cf392a986e495fdea
NadirAli90 <nadiraliunar360@gmail.com>
2023-12-21 03:54:37 -0500

    refactor: argument return for symbol or name

--- element.sh
+++ element.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 PSQL="psql --username=freecodecamp --dbname=periodic_table -t -c"
+
+NOT_FOUND () {
+    echo "$1"
+}
+
 if [[ -z $1 ]]; then
   echo "Please provide an element as an argument."
 else
@@ -7,12 +12,30 @@ else
     if [[ $1 =~ ^[0-9]+$ ]]; then
         DATA_QUERY=$($PSQL "SELECT p.atomic_number,p.atomic_mass,p.melting_point_celsius,p.boiling_point_celsius,t.type,e.symbol,e.name FROM properties AS p FULL JOIN elements AS e USING (atomic_number) FULL JOIN types AS t USING(type_id) WHERE p.atomic_number = $1;")
         IFS=" | " read -r ATOMIC_ID MASS MP BP TYPE SYMBOL NAME <<< $DATA_QUERY
-        echo "The element with atomic number $ATOMIC_ID is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MP celsius and a boiling point of $BP celsius." 
+        if [[ $ATOMIC_ID ]]; then
+         echo "The element with atomic number $ATOMIC_ID is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MP celsius and a boiling point of $BP celsius." 
+        else
+         NOT_FOUND "I could not find that element in the database."
 
-            if [[ -z $SYMBOL ]]; then
-            echo "I could not find that element in the database."
-            fi
-
+        fi
+    elif [[ $1 =~ ^[a-zA-Z]{1,2}$ ]]; then
+        DATA_QUERY=$($PSQL "SELECT p.atomic_number,p.atomic_mass,p.melting_point_celsius,p.boiling_point_celsius,t.type,e.symbol,e.name FROM properties AS p FULL JOIN elements AS e USING (atomic_number) FULL JOIN types AS t USING(type_id) WHERE e.symbol = '$1';")
+        IFS=" | " read -r ATOMIC_ID MASS MP BP TYPE SYMBOL NAME <<< $DATA_QUERY
+        if [[ $ATOMIC_ID ]]; then
+         echo "The element with atomic number $ATOMIC_ID is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MP celsius and a boiling point of $BP celsius." 
+        else
+         NOT_FOUND "I could not find that element in the database."
+         fi
+    elif [[ $1 =~ ^[a-zA-Z]+$ ]]; then
+        DATA_QUERY=$($PSQL "SELECT p.atomic_number,p.atomic_mass,p.melting_point_celsius,p.boiling_point_celsius,t.type,e.symbol,e.name FROM properties AS p FULL JOIN elements AS e USING (atomic_number) FULL JOIN types AS t USING(type_id) WHERE e.name = '$1';")
+        IFS=" | " read -r ATOMIC_ID MASS MP BP TYPE SYMBOL NAME <<< $DATA_QUERY
+        if [[ $ATOMIC_ID ]]; then
+         echo "The element with atomic number $ATOMIC_ID is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MP celsius and a boiling point of $BP celsius." 
+        else
+         NOT_FOUND "I could not find that element in the database."
+        fi
+    else 
+        NOT_FOUND "I could not find that element in the database."
     fi
 
 fi
